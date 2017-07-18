2ea6e5c78c58d2a6257de98da6b4ca6f73c5d4aa
bobgates <dvogt@polka.co.za>
2017-07-18 14:53:59 +0200

    Cleaned up initialization of borehole in main

--- src/main.rs
+++ src/main.rs
@@ -14,18 +14,13 @@ fn main() {
     borehole.set_step(0.5)
             .set_collar(0.0, 0.0, 0.0);
 
-    borehole.add_survey_obs(0.0,  0.0,  90.0);
-    borehole.add_survey_obs(10.0, 0.0, 0.0);
-    borehole.add_survey_obs(20.0, 180.0, 90.0);
-    borehole.add_survey_obs(30.0, 0.0, 0.0);
-    borehole.add_survey_obs(40.0, 90.0, 90.0);
-    borehole.add_survey_obs(50.0, 90.0, 0.0);
-    borehole.add_bottom_of_hole(60.);
-  
-
-    // for i in 0..5{
-    //     println!["{}: {}, {}, {}, {}", i, borehole.coords[i].depth, borehole.coords[i].x, borehole.coords[i].y, borehole.coords[i].z,]
-    // }
+    borehole.add_survey_obs(0.0,  0.0,  90.0)
+            .add_survey_obs(10.0, 0.0, 0.0)
+            .add_survey_obs(20.0, 180.0, 90.0)
+            .add_survey_obs(30.0, 0.0, 0.0)
+            .add_survey_obs(40.0, 90.0, 90.0)
+            .add_survey_obs(50.0, 90.0, 0.0)
+            .add_bottom_of_hole(60.);
 
     println!["depth: \tX, \tY, \tZ"];
     for depth in 0..61 {
